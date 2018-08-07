c4f7b75a867ef977e57728d2e2d85dcfcebc408d
github username <github email address>
2018-08-07 17:18:30 +0100

    Done.

---
+++ models/course.rb
@@ -0,0 +1,19 @@
+class Course
+
+    attr_reader :name, :topic
+
+    COURSES = []
+
+    def initialize(args)
+      @name = args[:name]
+      @topic = args[:topic]
+      COURSES <<
+    end
+
+
+    def self.all
+      COURSES
+    end
+
+
+end
